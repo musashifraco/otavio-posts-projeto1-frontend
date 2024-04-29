c1f3e40014930d6609f231fb021b40e4d1e9dd13
ezequiel.everton <ezequiel.everton@ceuma.br>
2024-04-29 15:16:36 -0300

    ultimo commit da secao 2

--- src/components/PostCard/PostCard.test.jsx
+++ src/components/PostCard/PostCard.test.jsx
@@ -1,24 +1,25 @@
-import { render, screen } from "@testing-library/react"
-import { PostCard } from "."
+import { render, screen } from "@testing-library/react";
+import { PostCard } from ".";
 
 const mock = {
-    id: 1,
-    title: 'title 1',
-    body: 'body 1', 
-    cover: 'img/img.png',
-}
+  id: 1,
+  title: "title 1",
+  body: "body 1",
+  cover: "img/img.png",
+};
 
-describe('<PostCard />', () => {
-    it('should render PostCard correctly', () => {
-        render(<PostCard {...mock}/>)
-        expect(screen.getByAltText(/title 1/i).toggleAttribute('img', 'img/img.png'))
-        expect(screen.getByRole('heading', {name: 'title 1 1'}))
-        expect(screen.getByText('body 1')).toBeInTheDocument()
+describe("<PostCard />", () => {
+  it("should render PostCard correctly", () => {
+    render(<PostCard {...mock} />);
+    expect(
+      screen.getByAltText(/title 1/i).toggleAttribute("img", "img/img.png")
+    );
+    expect(screen.getByRole("heading", { name: "title 1 1" }));
+    expect(screen.getByText("body 1")).toBeInTheDocument();
+  });
 
-    })
-
-    it('should match snapshot', () => {
-        const { container } = render(<PostCard {...mock} />)
-        expect(container.firstChild).toMatchSnapshot()
-    });
-})
+  it("should match snapshot", () => {
+    const { container } = render(<PostCard {...mock} />);
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
--- src/components/Posts/Post.test.jsx
+++ src/components/Posts/Post.test.jsx
@@ -1,49 +1,50 @@
-import { render, screen } from "@testing-library/react"
-import { Posts } from "."
+import { render, screen } from "@testing-library/react";
+import { Posts } from ".";
 
 const mock = {
-    posts: [
-        {
-            id: 1,
-            title: 'title 1',
-            body: 'body 1',
-            cover: 'img/img1.png',
-        },
-        {
-            id: 2,
-            title: 'title 2',
-            body: 'body 2',
-            cover: 'img/img2.png',
-        },
-        {
-            id: 3,
-            title: 'title 3',
-            body: 'body 3',
-            cover: 'img/img3.png',
-        },
-    ]
+  posts: [
+    {
+      id: 1,
+      title: "title 1",
+      body: "body 1",
+      cover: "img/img1.png",
+    },
+    {
+      id: 2,
+      title: "title 2",
+      body: "body 2",
+      cover: "img/img2.png",
+    },
+    {
+      id: 3,
+      title: "title 3",
+      body: "body 3",
+      cover: "img/img3.png",
+    },
+  ],
+};
 
+describe("<Posts />", () => {
+  it("should render posts", () => {
+    render(<Posts {...mock} />);
+    expect(screen.getAllByRole("heading", { name: /title/i })).toHaveLength(3);
+    expect(screen.getAllByRole("img", { name: /title/i })).toHaveLength(3);
+    expect(screen.getAllByText(/body/i)).toHaveLength(3);
+  });
 
-}
+  if (
+    ("should not render posts",
+    () => {
+      render(<Posts />);
 
-describe('<Posts />', () => {
-    it('should render Post correctly', () => {
-        render(<Posts />)
-
-        expect(screen.queryByRole('heading', {name: /title/i})).not.toBeInTheDocument()
-        // ou
-        expect(screen.queryAllByRole('heading', {name: /title/i})).toHaveLength(0)
+      expect(
+        screen.queryByRole("heading", { name: /title/i })
+      ).not.toBeInTheDocument();
     })
-    it('should render Post correctly', () => {
-        render(<Posts {...mock} />)
-        expect(screen.getAllByRole('heading', {name: /title/i})).toHaveLength(3)
-        expect(screen.getAllByRole('img', {name: /title/i})).toHaveLength(3)
-        expect(screen.getAllByText(/body/i)).toHaveLength(3)
-    })
-
-    it('should match snapshot', () => {
-        const { container } = render(<Posts {...mock} />)
-        expect(container.firstChild).toMatchSnapshot();
-    })
-})
-
+  )
+    it("should match snapshot", () => {
+      const { container } = render(<Posts {...mock} />);
+      // eslint-disable-next-line testing-library/no-node-access
+      expect(container.firstChild).toMatchSnapshot();
+    });
+});
